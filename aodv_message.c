761ca94ef687c2f4c50a6ee6087da9264f5a0e37
Viet Hoang <vincenthp2603@gmail.com>
2023-02-09 23:18:10 +0100

    RREP looks okay

--- aodv_message.c
+++ aodv_message.c
@@ -1,7 +1,10 @@
+#include "contiki.h"
+#include "net/rime/rime.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "aodv_message.h"
+#include "aodv_table.h"
 
 void clean_msg_buffer(char *msg)
 {
@@ -24,6 +27,8 @@ int parse_message_type(char* msg)
         return R_REQ;
     } else if (strcmp(msg_type_str, "RREP") == 0) {
         return R_REP;
+    } else if (strcmp(msg_type_str, "RERR") == 0) {
+        return R_ERR;
     }
     
     return R_REP_ACK;
@@ -103,7 +108,7 @@ struct RREP_message RREP_parse(char* msg)
                 rrep_msg.dest = atoi(token);
                 break;
             default:
-                strcpy(rrep_msg.path, token);
+                rrep_msg.hop_to_dest = atoi(token);
         }
     }
     return rrep_msg;
@@ -126,68 +131,123 @@ void RREP_stringify(struct RREP_message msg, char *msg_str)
     strcat(msg_str, dest_str);
     strcat(msg_str, "_");
 
-    strcat(msg_str, msg.path); 
-}
-
-void RREP_add_node_to_path(struct RREP_message *msg, int node)
-{
-    char old_path[40];
-    char new_path[50];
-   
-    strcpy(old_path, msg->path);
-    new_path[0] = (char) node + 48;
-    new_path[1] = '-';
-    strcat(new_path, old_path);
-    strcpy(msg->path, new_path);
+    char hop_str[2];
+    hop_str[1] = '\0';
+    hop_str[0] = (char) msg.hop_to_dest + 48;
+    strcat(msg_str, hop_str); 
 }
 
 /*
     RREPACK
 */
-struct RREPACK_message RREPACK_parse(char* msg)
-{
-    struct RREPACK_message rrepack_msg;
-    const char* delim = "_";
-    char str[30];
-    strcpy(str, msg);
+// struct RREPACK_message RREPACK_parse(char* msg)
+// {
+//     struct RREPACK_message rrepack_msg;
+//     const char* delim = "_";
+//     char str[30];
+//     strcpy(str, msg);
 
-    char *token = strtok(str, delim);
-    int i = 0;
-    for (i = 0; i < 3; i++) {
-        token = strtok(NULL, "_");
-        switch (i) {
-            case 0:
-                rrepack_msg.start = atoi(token);
-                break;
-            case 1:
-                rrepack_msg.dest = atoi(token);
-                break;
-            default:
-                rrepack_msg.rrep_receiver = atoi(token);
-        }
-    }
-    return rrepack_msg;
+//     char *token = strtok(str, delim);
+//     int i = 0;
+//     for (i = 0; i < 2; i++) {
+//         token = strtok(NULL, "_");
+//         switch (i) {
+//             case 0:
+//                 rrepack_msg.start = atoi(token);
+//                 break;
+//             case 1:
+//                 rrepack_msg.dest = atoi(token);
+//                 break;
+//             default:
+//                 rrepack_msg.rrep_receiver = atoi(token);
+//         }
+//     }
+//     return rrepack_msg;
+// }
+
+// void RREPACK_stringify(struct RREPACK_message msg, char *msg_str)
+// {
+//     clean_msg_buffer(msg_str);
+//     strcat(msg_str, "RREPACK_");
+
+//     char start_str[2];
+//     start_str[1] = '\0';
+//     start_str[0] = (char) msg.start + 48; // ascii hack
+//     strcat(msg_str, start_str);
+//     strcat(msg_str, "_");    
+
+//     char dest_str[2];
+//     dest_str[1] = '\0';
+//     dest_str[0] = (char) msg.dest + 48;
+//     strcat(msg_str, dest_str);
+//     strcat(msg_str, "_");
+
+//     char rrep_receiver_str[2];
+//     rrep_receiver_str[1] = '\0';
+//     rrep_receiver_str[0] = (char) msg.rrep_receiver + 48;
+//     strcat(msg_str, rrep_receiver_str); 
+// }
+
+/*
+    RREPACK
+*/
+
+
+/*
+    UTILS FUNCTIONS
+*/
+
+void rreq_forward(struct broadcast_conn *c, struct RREQ_message msg)
+{
+    aodv_print_table();
+    msg.hop_to_start = msg.hop_to_start + 1;
+
+    char msg_str[30];
+    RREQ_stringify(msg, msg_str);
+    packetbuf_copyfrom(msg_str, strlen(msg_str));
+
+    broadcast_send(c);
 }
 
-void RREPACK_stringify(struct RREPACK_message msg, char *msg_str)
+// void rrep_node_down_handler(void *data)
+// {
+//     int next_node = *((int*) data);
+//     if (neighbor_is_up[next_node])
+//     {
+//         printf("Node %d seems alive.\n", next_node);
+//     }
+//     else
+//     {
+//         printf("Node %d seems dead.\n", next_node);
+//     }
+// }
+
+void rrep_forward(struct unicast_conn *c, struct RREP_message msg)
 {
-    clean_msg_buffer(msg_str);
-    strcat(msg_str, "RREPACK_");
+    int start = msg.start;
+    struct aodv_table_entry *entry = aodv_get_best_entry(start);
 
-    char start_str[2];
-    start_str[1] = '\0';
-    start_str[0] = (char) msg.start + 48; // ascii hack
-    strcat(msg_str, start_str);
-    strcat(msg_str, "_");    
+    if (entry)
+    {
+        linkaddr_t addr;
+        addr.u8[0] = entry->next_node;
+        addr.u8[1] = 0;
 
-    char dest_str[2];
-    dest_str[1] = '\0';
-    dest_str[0] = (char) msg.dest + 48;
-    strcat(msg_str, dest_str);
-    strcat(msg_str, "_");
+        msg.hop_to_dest = msg.hop_to_dest + 1;
 
-    char rrep_receiver_str[2];
-    rrep_receiver_str[1] = '\0';
-    rrep_receiver_str[0] = (char) msg.rrep_receiver + 48;
-    strcat(msg_str, rrep_receiver_str); 
+        char msg_str[30];
+        RREP_stringify(msg, msg_str);
+        packetbuf_copyfrom(msg_str, strlen(msg_str)+1);
+
+        unicast_send(c, &addr);
+
+        //neighbor_is_up[entry->next_node] = 0;
+        //ctimer_set(&cb_timer, 3 * CLOCK_SECOND, rrep_node_down_handler, &entry->next_node);        
+    }
+}
+
+void rrepack_send(struct unicast_conn *c, const linkaddr_t *rrep_sender)
+{
+    packetbuf_copyfrom("RREPACK", 8);
+    unicast_send(c, rrep_sender);
 }
--- aodv_message.h
+++ aodv_message.h
@@ -1,6 +1,7 @@
 #define R_REQ 0
 #define R_REP 1
 #define R_REP_ACK 2
+#define R_ERR 3
 
 struct RREQ_message {
     int start;
@@ -11,13 +12,17 @@ struct RREQ_message {
 struct RREP_message {
     int start;
     int dest;
-    char path[30];
+    int hop_to_dest;
 };
 
 struct RREPACK_message {
     int start;
     int dest;
-    int rrep_receiver;
+};
+
+struct RERR_message {
+    int start;
+    int dest;
 };
 
 int parse_message_type(char* msg);
@@ -30,8 +35,12 @@ struct RREP_message RREP_parse(char* msg);
 
 void RREP_stringify(struct RREP_message msg, char *msg_str);
 
-void RREP_add_node_to_path(struct RREP_message *msg, int node);
+// struct RREPACK_message RREPACK_parse(char* msg);
 
-struct RREPACK_message RREPACK_parse(char* msg);
+// void RREPACK_stringify(struct RREPACK_message msg, char *msg_str);
 
-void RREPACK_stringify(struct RREPACK_message msg, char *msg_str);
+void rreq_forward(struct broadcast_conn *c, struct RREQ_message msg);
+
+void rrep_forward(struct unicast_conn *c, struct RREP_message msg);
+
+void rrepack_send(struct unicast_conn *c, const linkaddr_t *rrep_sender);
